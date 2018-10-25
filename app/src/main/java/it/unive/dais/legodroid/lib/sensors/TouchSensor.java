70de649ef29e13e3978273702d1b64ae7e9e217c
Alvise Spanò <spano.alvise@gmail.com>
2018-10-25 15:39:15 +0200

    Revert "Added right sensors commands"
    This reverts commit 175fa83c678d25f8f15f8190c0e5c402141e549a.

--- app/src/main/java/it/unive/dais/legodroid/lib/sensors/TouchSensor.java
+++ app/src/main/java/it/unive/dais/legodroid/lib/sensors/TouchSensor.java
@@ -1,13 +1,23 @@
 package it.unive.dais.legodroid.lib.sensors;
 
 import it.unive.dais.legodroid.lib.EV3;
+import it.unive.dais.legodroid.lib.util.Consumer;
+import it.unive.dais.legodroid.lib.util.Promise;
+
+import java.io.IOException;
+
+import static it.unive.dais.legodroid.lib.comm.Constants.EV3_TOUCH;
+import static it.unive.dais.legodroid.lib.comm.Constants.TOUCH_TOUCH;
+
 
 public class TouchSensor extends AbstractSensor {
     public TouchSensor(EV3 ev3, int port) {
         super(ev3, port);
     }
 
-    public boolean getPressed() {
-        return false;
+    public Promise<Boolean> getPressed() throws IOException {
+        final Promise<Boolean> promise = new Promise<>();  // TODO: da rifare
+//        ev3.getSiValue(port, EV3_TOUCH, TOUCH_TOUCH, 1).then((Consumer<float[]>) values -> promise.resolve((int) values[0] == 1));
+        return promise;
     }
 }
