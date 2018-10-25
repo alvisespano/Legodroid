175fa83c678d25f8f15f8190c0e5c402141e549a
giulioz <mail.zausa.giulio@gmail.com>
2018-10-25 12:05:47 +0200

    Added right sensors commands

--- app/src/main/java/it/unive/dais/legodroid/lib/sensors/GyroSensor.java
+++ app/src/main/java/it/unive/dais/legodroid/lib/sensors/GyroSensor.java
@@ -1,23 +1,21 @@
 package it.unive.dais.legodroid.lib.sensors;
 
 import it.unive.dais.legodroid.lib.EV3;
-import it.unive.dais.legodroid.lib.util.Consumer;
-import it.unive.dais.legodroid.lib.util.Promise;
-
-import java.io.IOException;
-
-import static it.unive.dais.legodroid.lib.comm.Constants.EV3_TOUCH;
-import static it.unive.dais.legodroid.lib.comm.Constants.TOUCH_TOUCH;
-
 
 public class GyroSensor extends AbstractSensor {
     public GyroSensor(EV3 ev3, int port) {
         super(ev3, port);
     }
 
-    public Promise<Boolean> getPressed() throws IOException {
-        final Promise<Boolean> promise = new Promise<>();  // TODO: da rifare
-//        ev3.getSiValue(port, EV3_TOUCH, TOUCH_TOUCH, 1).then((Consumer<float[]>) values -> promise.resolve((int) values[0] == 1));
-        return promise;
+    public void calibrate() {
+
+    }
+
+    public int getAngle() {
+        return 0;
+    }
+
+    public int getRotSpeed() {
+        return 0;
     }
 }
